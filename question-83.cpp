53800b580ac2d2f8970f0d9d2ad6d6f1befd76bc
dotseema <99743625+dotseema@users.noreply.github.com>
2022-04-07 09:05:28 +0530

    Add files via upload

---
+++ question-83.cpp
@@ -0,0 +1,28 @@
+//Write a C++ program to convert a given number into hours and minutes.
+//Separate the number of hours and minutes with a colon
+#include <iostream>
+#include <string>
+using namespace std;
+void Time_Convert(int num) {
+	bool flag;
+	int hr = 0;
+	do
+	{
+		flag = false;
+		if (num >= 60)
+		{
+			hr++;
+			num -= 60;
+			flag = true;
+		}
+	} while (flag);
+	cout << "\nH:M " << hr << ":" << num << endl;
+}
+
+int main() {
+	Time_Convert(67);
+	Time_Convert(60);
+	Time_Convert(120);
+	Time_Convert(40);
+	return 0;
+}
